cc7f44bbe03b5e116a2d95afbcd3ee13ee81ba29
Iryna <IrynaVdovychenko2012@gmail.com>
2025-03-21 10:20:22 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
 "use strict"
 
-
-$(document).ready(function() {
-    $('.slider').slick({
-        arrows:false,
-        dots:true,
-        adaptiveHeight:false,
-        waitForAnimate:false,
-    });
-});
-
 /* -----------------Меню Бургер---------------------- */
 
 const iconMenu = document.querySelector('.menu__icon');
@@ -23,33 +13,3 @@ if (iconMenu) {
 };
 
 /* -----------------Меню Бургер---------------------- */
-
-/* ---------------Прокрутка до розділу--------------- */
-
-const menuLinks = document.querySelectorAll('.menu__item[data-goto]');
-if (menuLinks.length >0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener('click', function (e) {
-            const menuLink = e.target;
-            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
-                const gotoBlock = document.querySelector(menuLink.dataset.goto);
-                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
-                
-                //Закриття меню Бургер
-                if(iconMenu.classList.contains('_active')) {
-                    document.body.classList.remove('_lock');
-                    iconMenu.classList.remove('_active');
-                    menuBody.classList.remove('_active');
-                }
-
-                window.scrollTo({
-                    top: gotoBlockValue,
-                    behavior: "smooth"
-                });
-                e.preventDefault();
-            }
-        });
-    });
-};
-
-/* ---------------Прокрутка до розділу--------------- */
